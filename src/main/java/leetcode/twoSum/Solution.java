740107f17f278c12d9fa47f5f248336d7f21fc17
e <xsxiao2018@126.com>
2019-09-11 08:46:21 +0800

    Initial commit

---
+++ src/main/java/leetcode/twoSum/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.twoSum;
+
+// 暴力法
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int complement;
+        for (int x = 0; x<nums.length; x++) {
+            complement = target - nums[x];
+            for (int y = 0; y<nums.length; y++) {
+                if (x ==  y) { continue; }
+                if (nums[y] == complement) {
+                    return new int[] {x, y};
+                }
+            }
+        }
+        return new int[] {0, 0};
+    }
+}
---
+++ src/test/java/leetcode/twoSum/SolutionTest.java
@@ -0,0 +1,36 @@
+package leetcode.twoSum; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+import org.junit.Assert.*;
+
+/** 
+* Solution Tester. 
+* 
+* @author 
+* @version 1.0 
+*/ 
+public class SolutionTest { 
+
+    @Before
+    public void before() throws Exception { 
+    } 
+    
+    @After
+    public void after() throws Exception { 
+    } 
+    
+        /** 
+    * 
+    * Method: twoSum(int[] nums, int target) 
+    * 
+    */ 
+    @Test
+    public void testTwoSum() throws Exception { 
+    //TODO: Test goes here... 
+    } 
+    
+        
+    } 
